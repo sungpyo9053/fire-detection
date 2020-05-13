a1f89f4c8dad6d0d78138525f4614d62cb012dd9
sungpyo9053 <44603374+sungpyo9053@users.noreply.github.com>
2020-05-14 00:55:28 +0900

    Create resnet50dectection.py

---
+++ resnet50dectection.py
@@ -0,0 +1,144 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)'
+import matplotlib.pyplot as plt
+from tensorflow.python.keras.applications import ResNet50
+from tensorflow.python.keras.applications import VGG16
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, Flatten, GlobalAveragePooling2D
+from tensorflow.python.keras.applications.resnet50 import preprocess_input
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+from sklearn.utils import class_weight
+from tensorflow.python.keras import optimizers
+
+
+import cv2
+import math
+from IPython.display import clear_output
+
+
+import os
+print(os.listdir("../input/fire-detection-from-cctv/data/data/img_data"))
+print(os.listdir("../input/fire-detection-from-cctv/data/data/video_data/test_videos"))
+print(os.listdir("../input"))
+
+print(os.listdir("../input/resnet50"))
+print(os.listdir("../input/vgg16"))
+
+ 
+# 데이터 크기
+IMG_SIZE = 224
+NUM_EPOCHS = 20 # 20
+NUM_CLASSES = 3
+TRAIN_BATCH_SIZE = 20 # 77
+TEST_BATCH_SIZE = 1 
+
+# 모델 함수
+def create_model( model_size ):
+    my_new_model = Sequential()
+    if  model_size == 'L':
+        resnet_weights_path = '../input/resnet50/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
+        resnet = ResNet50(include_top=False, pooling='avg', weights=resnet_weights_path)
+        #resnet.summary()
+        my_new_model.add(resnet)
+        my_new_model.layers[0].trainable = False
+    else:
+        vgg_weights_path = '../input/vgg16/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
+        vgg= VGG16(include_top=False, weights=vgg_weights_path ) 
+        vgg.summary()
+        my_new_model.add(vgg)
+        my_new_model.add(GlobalAveragePooling2D())
+        my_new_model.layers[0].trainable = False
+        my_new_model.layers[1].trainable = False
+        
+    my_new_model.add(Dense(NUM_CLASSES, activation='softmax'))
+   
+    # Say no to train first layer (ResNet) model. It is already trained
+    
+    opt = optimizers.adam()
+    my_new_model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+    return my_new_model
+    
+   # 훈련 모델
+   def train_model( model ):
+
+    data_generator_with_aug = ImageDataGenerator(preprocessing_function=preprocess_input,
+                                width_shift_range=0.1,
+                                height_shift_range=0.1,
+                                #sear_range=0.01,
+                                zoom_range=[0.9, 1.25],
+                                horizontal_flip=True,
+                                vertical_flip=False,
+                                data_format='channels_last',
+                                brightness_range=[0.5, 1.5]
+                               )
+                                       
+    train_generator = data_generator_with_aug.flow_from_directory(
+            '../input/fire-detection-from-cctv/data/data/img_data/train',
+            target_size=(IMG_SIZE, IMG_SIZE),
+            batch_size=TRAIN_BATCH_SIZE,
+            class_mode='categorical')
+   
+   # 예측 그래프 
+   def draw_prediction( frame, class_string ):
+    x_start = frame.shape[1] -600
+    cv2.putText(frame, class_string, (x_start, 75), cv2.FONT_HERSHEY_SIMPLEX, 2.5, (255, 0, 0), 2, cv2.LINE_AA)
+    return frame
+   
+    validation_generator = data_generator_with_aug.flow_from_directory(
+            '../input/fire-detection-from-cctv/data/data/img_data/test',
+            target_size=(IMG_SIZE, IMG_SIZE),
+            batch_size=TEST_BATCH_SIZE,
+            shuffle = False,
+            class_mode='categorical')
+    
+    H = model.fit_generator(
+            train_generator,
+            steps_per_epoch=train_generator.n/TRAIN_BATCH_SIZE,
+            epochs=NUM_EPOCHS,
+            validation_data=validation_generator,
+            validation_steps=1 #,
+            #class_weight=dict_weights
+                )
+    
+    plot_history( H, NUM_EPOCHS )
+    
+    return model, train_generator,validation_generator
+    
+    # 예측
+    def predict(  model, video_path, filename, label_dict ):
+    
+    vs = cv2.VideoCapture(video_path)
+    fps = math.floor(vs.get(cv2.CAP_PROP_FPS))
+    ret_val = True
+    writer = 0
+    
+    while True:
+        ret_val, frame = vs.read()
+        if not ret_val:
+            break
+       
+        resized_frame = cv2.resize(frame, (IMG_SIZE, IMG_SIZE))
+        frame_for_pred = prepare_image_for_prediction( resized_frame )
+        pred_vec = model.predict(frame_for_pred)
+        #print(pred_vec)
+        pred_class =[]
+        confidence = np.round(pred_vec.max(),2) 
+        
+        if confidence > 0.4:
+            pc = pred_vec.argmax()
+            pred_class.append( (pc, confidence) )
+        else:
+            pred_class.append( (0, 0) )
+        if pred_class:
+            txt = get_display_string(pred_class, label_dict)       
+            frame = draw_prediction( frame, txt )
+        if not writer:
+            fourcc = cv2.VideoWriter_fourcc(*"XVID")
+            writer = cv2.VideoWriter(filename, fourcc, fps,(frame.shape[1], frame.shape[0]), True)
+            
+        writer.write(frame)
+        
+    vs.release()
+    writer.release()
+      
+   
